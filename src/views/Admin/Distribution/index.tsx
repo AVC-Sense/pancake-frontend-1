ce3027c9b6d12b5f587a86d9e168270655f2b799
Omar <rand@rand.com>
2022-04-12 09:24:40 -0400

    new test cnt

--- src/views/Admin/Distribution/index.tsx
+++ src/views/Admin/Distribution/index.tsx
@@ -58,17 +58,6 @@ export default function DistributionCard() {
     console.log('do nothing')
   }, [])
 
-  const hexToString = () => {
-    hex_to_ascii(str1)
-
-    const hex = str1.toString()
-    let str = ''
-    for (let n = 0; n < hex.length; n += 2) {
-      str += String.fromCharCode(parseInt(hex.substr(n, 2), 16))
-    }
-    return str
-  }
-
   const { independentField, typedValue, recipient } = useSwapState()
   const dependentField: Field = independentField === Field.INPUT ? Field.OUTPUT : Field.INPUT
 
